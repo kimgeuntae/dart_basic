c2eca9cfdc74f39b74a1abf0cf597989d093e8a0
geuntae.kim <kok617@naver.com>
2022-10-02 23:46:07 +0900

    main 함수에도 async await 을 줄수 있음 await 을 줘서 addNumbers 가 순서대로 작동되는 것을 확인

--- dart_Async_Programming.dart
+++ dart_Async_Programming.dart
@@ -1,15 +1,16 @@
-void main() {
+void main() async {
   // Future - 미래
   // 미래에 받아올 값
   Future<String> name = Future.value('코드팩토리');
   Future<int> number = Future.value(1);
   Future<bool> isTrue = Future.value(true);
 
-  addNumbers1(1, 1);
-  addNumbers2(2, 2);
+  await addNumbers(1, 1);
+  await addNumbers(2, 2);
 }
 
 // await 이 없는 경우, delayed 이후 코드 실행
+/*
 void addNumbers1(int num1, int num2) {
   print('계산 시작1 : $num1 + $num2');
 
@@ -20,15 +21,15 @@ void addNumbers1(int num1, int num2) {
 
   print('함수 완료1 : $num1 + $num2');
 }
-
+*/
 // await 이 있는 경우, async 함수 내의 delayed 이후 코드를 기다림
-void addNumbers2(int num1, int num2) async {
-  print('계산 시작2 : $num1 + $num2');
+Future<void> addNumbers(int num1, int num2) async {
+  print('계산 시작 : $num1 + $num2');
 
   // 서버 시뮬레이션
   await Future.delayed(Duration(seconds: 2), () {
-    print('계산 완료2 : $num1 + $num2 = ${num1 + num2}');
+    print('계산 완료 : $num1 + $num2 = ${num1 + num2}');
   });
 
-  print('함수 완료2 : $num1 + $num2');
+  print('함수 완료 : $num1 + $num2');
 }
