1a5ddaa1792bde78175fe2f564c58fe6401aec18
geuntae.kim <kok617@naver.com>
2022-10-02 16:25:44 +0900

    dart OOP

---
+++ dart_OOP.dart
@@ -0,0 +1,13 @@
+void main() {
+  Test test1 = Test();
+
+  // Object에서 상속받음
+  // test1.hashCode
+  // test1.runtimeType
+  // test1.toString
+  // test1.noSuchMethod
+}
+
+// Object Oriented Programming (OOP)
+// class test extends Object{}
+class Test {} // extends 를 적지않아도 기본적으로 Object를 상속받음
