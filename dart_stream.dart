0218a24f28f9b4e502561ba3d85a472119c2c1ad
geuntae.kim <kok617@naver.com>
2022-10-03 23:55:18 +0900

    stream listener 데이터 입력 시 변환 예제 where 필터링 사용

--- dart_stream.dart
+++ dart_stream.dart
@@ -6,13 +6,14 @@ void main() {
   final stream = controller.stream.asBroadcastStream(); // 브로드캐스트 스트림화
   
   // 파라메터 함수를 listen하고 있을때 값이 들어가면 함수 실행 
-  final streamListener1 = stream.listen((val){
+  final streamListener1 = stream.where((val) => val % 2 == 0).listen((val){
     print('Listener1 : $val');
   });
   
-  final streamListener2 = stream.listen((val){
+  final streamListener2 = stream.where((val) => val % 2 == 1).listen((val){
     print('Listener2 : $val');
   });
+  // 리스너 데이터 변환 예제 where 사용
   
   // add 를 통해 리슨하는 함수에 전달
   controller.sink.add(1);
