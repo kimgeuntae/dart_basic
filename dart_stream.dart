e0b1a77ab2f8c5b0648b8e5e884a78237e8eaa3e
geuntae.kim <kok617@naver.com>
2022-10-03 23:53:14 +0900

    asbroadcastStream 함수를 사용한 다중 리스너 사용 예제

--- dart_stream.dart
+++ dart_stream.dart
@@ -2,11 +2,16 @@ import 'dart:async';
 
 void main() {
   final controller = StreamController();
-  final stream = controller.stream;
+  // final stream = controller.stream; //단일
+  final stream = controller.stream.asBroadcastStream(); // 브로드캐스트 스트림화
   
   // 파라메터 함수를 listen하고 있을때 값이 들어가면 함수 실행 
   final streamListener1 = stream.listen((val){
     print('Listener1 : $val');
+  });
+  
+  final streamListener2 = stream.listen((val){
+    print('Listener2 : $val');
   });
   
   // add 를 통해 리슨하는 함수에 전달
